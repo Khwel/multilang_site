f12e82d54b568e001729ae4afa4fb69c4f2a190b
Khwel <erwan1904@live.fr>
2024-06-24 15:29:02 +0200

    test 5

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-slz62p(3k@s85*ljqf0&ihhi&7x60-tppszkmvyh3(k=da=w$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://multilang-site-0kom.onrender.com/']
 
 
 # Application definition
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
